be2f6725b53fb7f57f54717571b4ef637f759c75
Shubham-Pujan <shubhampujan@gmail.com>
2018-02-06 11:24:59 +0530

    data structures

---
+++ data_structures.py
@@ -0,0 +1,40 @@
+
+l=[1,2,"a"]
+t=(1,2,"a")
+t
+t[1]
+l[1]
+d={"a":1,"b":2}
+
+
+set1={1,2,3}
+frozensets1=frozenset({1,2,3})
+frozensets1
+type(frozensets1)
+type(l)
+
+
+b="nice"
+a="ls"
+mylist=["my","list",a,b]
+mylist2=[[4,5,6,7],[3,4,5,6]]
+mylist[2]
+len(mylist)
+mylist[1:3]
+mylist2[1]
+mylist2[1][:2]
+mylist+mylist
+mylist2[0][1]>4
+
+for i in mylist2[1]:
+    print(i)
+    if i > 4:
+        print ( "true")
+    else:
+        print ("false")
+mylist.remove(a)
+mylist
+mylist.pop()
+mylist
+mylist.count("my")
+
